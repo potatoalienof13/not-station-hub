1ac379e74708db9fcbccb9263f78a6c341f9babc
potatoalienof13 <potatoalienof13@gmail.com>
2021-08-22 20:06:11 -0400

    Logins work, and fix new lines in station names

--- nsh/nsh.py
+++ nsh/nsh.py
@@ -1,5 +1,5 @@
 from simple_term_menu import TerminalMenu
-#import pyrebase
+import pyrebase
 import os
 from pathlib import Path
 from .server import ServerHub
@@ -10,17 +10,23 @@ class Nsh:
     config_dir: Path
 
     def __init__(self):
-        # firebase_config = {  # todo, fill this out with stuff that actually works
-        #    "apiKey": "apiKey",
-        #    "authDomain": "projectId.firebaseapp.com",
-        #    "databaseURL": "https://databaseName.firebaseio.com",
-        #    "storageBucket": "projectId.appspot.com"
-        # }
-        # self.firebase = pyrbase.initialize_app(firebase_config)
+        firebase_config = {  # todo, fill this out with stuff that actually works
+            "apiKey": "AIzaSyB7GorzPgwHYjSV4XaJoszj98tLM4_WZpE",
+            "authDomain": "projectId.firebaseapp.com",
+            "databaseURL": "https://api.unitystation.org",
+            "storageBucket": "projectId.appspot.com"
+        }
+        
+        self.firebase = pyrebase.initialize_app(firebase_config)
+
         self.config_dir = self.get_path_from_env_or_default(
             "XDG_CONFIG_HOME", Path.home() / ".config", "nsh")
         self.install_dir = self.get_path_from_env_or_default(
             "NSH_INSTALL_DIR", Path.home() / ".local" / "nsh", "")
+
+
+        self.auth = self.firebase.auth()
+        self.user = self.auth.sign_in_with_email_and_password(input("email:"), input( "password:"))
 
     def get_path_from_env_or_default(self, env_var_name: str, backup_path: Path, suffix: Path):
         if env_var := os.getenv(env_var_name):
@@ -34,7 +40,6 @@ class Nsh:
     def run(self):
         hub = ServerHub(
             "https://api.unitystation.org/serverlist", self.install_dir)
-
-        menu = TerminalMenu([x.name for x in hub.servers])
+        menu = TerminalMenu([x.name.replace("\n", " ") for x in hub.servers])
         index = menu.show()
-        hub.servers[index].run("", "")
+        hub.servers[index].run(self.user["refreshToken"], self.user["localId"])
